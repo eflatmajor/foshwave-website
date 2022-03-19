cbd47ae7bb897bbc65b8cf2c0c3ed260fbe77625
eflatmajor <bachbeethovenundbrahms@gmail.com>
2022-03-19 00:02:08 +0000

    Fix link colours.

--- pages/artists/_id.vue
+++ pages/artists/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dual-section">
+  <div class="dual-section" :style="cssVariables">
     <section class="left-section">
       <h2 :class="colours">
         Socials
@@ -76,6 +76,30 @@
 
 <script>
 export default {
+  data() {
+    return {
+      colours: {
+        "red": "#EF4444",
+        "orange": "#F97316",
+        "amber": "#F59E0B",
+        "yellow": "#EAB308",
+        "lime": "#84CC16",
+        "green": "#22C55E",
+        "emerald": "#10B981",
+        "teal": "#14B8A6",
+        "cyan": "#06B6D4",
+        "sky": "#0EA5E9",
+        "blue": "#3B82F6",
+        "indigo": "#6366F1",
+        "violet": "#8B5CF6",
+        "purple": "#A855F7",
+        "fuchsia": "#D946EF",
+        "pink": "#EC4899",
+        "rose": "#F43F5E"
+      }
+    }
+  },
+
   async asyncData ({ $content, params, error }) {
     const artist = await $content('artists')
       .where({ slug: { $eq: params.id } })
@@ -98,6 +122,12 @@ export default {
   },
 
   computed: {
+    cssVariables() {
+      return {
+        "--text-colour": this.colours[this.artist.colour]
+      };
+    },
+
     aliases() {
       return this.artist.aliases.join(', ')
     },
@@ -233,11 +263,11 @@ div.nuxt-content h3 {
 }
 
 div.nuxt-content p {
-  color: white;
+  color: white !important;
 }
 
 div.nuxt-content p a {
+  color: var(--text-colour);
   font-weight: bold;
-  text-decoration: underline;
 }
 </style>
